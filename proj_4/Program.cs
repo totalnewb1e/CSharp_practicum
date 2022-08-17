9199cb10ad2058129c940c7d6502d21100f15429
Val <valprado.v1@gmail.com>
2022-08-17 18:29:20 +0300

    proj_4 sum of a random nums

---
+++ proj_4/Program.cs
@@ -0,0 +1,6 @@
+﻿int numA = new Random().Next(1, 25); // nums from 1 to 25
+Console.WriteLine(numA);
+int numB = new Random().Next(1, 25); // nums from 1 to 25
+Console.WriteLine(numB);
+int result = numA + numB;
+Console.WriteLine(result);
