cb56bd0b197759c0e98bfce68872c31a02df8f76
Val <valprado.v1@gmail.com>
2022-08-17 18:50:20 +0300

    proj_5 if else + to lower

---
+++ proj_5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter ur name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "petr")
+{
+    Console.Write("Surprise mothafucka the king is back!");
+}
+else
+{
+    Console.Write("HI, ");
+    Console.Write(username);
+}
